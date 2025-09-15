080d4bea87ff81c282685f3c38b28030fb7d65a5
Axel <148916949+oliverexx@users.noreply.github.com>
2025-09-15 18:23:27 -0300

    Update Ad_guide.sh

--- Ad_guide.sh
+++ Ad_guide.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Active Directory Pentesting - Guía Completa Expandida
-# Incluye técnicas avanzadas de AD CS, Shadow Credentials, NoPac, Delegación Kerberos, etc.
+# Active Directory Pentesting
 
-# Colores para la interfaz
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
